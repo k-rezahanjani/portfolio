0c9b5ac71083a6e9afb161139b6a535b3e2c9486
k-rezahanjani <kaveh.rh69@gmail.com>
2023-11-04 00:22:51 +0100

    fixes sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,16 @@
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {schemaTypes} from '../portfolio/sanity/schemas/index';
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import {schemaTypes} from "../portfolio/sanity/schemas/index";
 
-export default defineConfig({
-  basePath: '/studio',
-  projectId: 'rl5a80u6',
-  dataset: 'production',
-  schemaTypes,
-  apitVersion: '2021-03-25',
-  plugins: [
-    deskTool(),
-  ],
-})
+
+const config =  defineConfig({
+    projectId: 'p0ipwu50',
+    dataset: 'production',
+    title: 'My Personal Portfolio',
+    apiVersion: '2023-05-04',
+    basePath: '/admin',
+    plugins: [deskTool()],
+    schema: {types: schemaTypes}
+  })
+
+  export default config;
